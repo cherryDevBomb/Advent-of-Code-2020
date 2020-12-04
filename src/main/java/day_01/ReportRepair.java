a9be9d8ad216d9f71e4e62747539572d575a5684
cherryDevBomb <turcan.olguta@gmail.com>
2020-12-04 19:06:38 +0200

    complete day 1

--- src/main/java/day_01/ReportRepair.java
+++ src/main/java/day_01/ReportRepair.java
@@ -1,4 +1,54 @@
 package day_01;
 
+import util.InputReader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReportRepair {
+
+    private static final String INPUT_FILE = "day_01/input.txt";
+
+    public static void main(String[] args) {
+        new ReportRepair().repairReport();
+    }
+
+    private void repairReport() {
+        List<Integer> numbers = readInput();
+        System.out.println("Product of 2 numbers that sum to 2020: " + getResultForTwoNumbers(numbers));
+        System.out.println("Product of 3 numbers that sum to 2020: " + getResultForThreeNumbers(numbers));
+    }
+
+    private List<Integer> readInput() {
+        return InputReader.readInputFile(INPUT_FILE)
+                .stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    /***
+     * Find two entries in a list that sum to 2020 and then multiply those two numbers together.
+     *
+     * @param numbers List of Integer
+     * @return product of those two numbers
+     */
+    private int getResultForTwoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(nr1 -> numbers.stream().anyMatch(nr2 -> (nr1 + nr2 == 2020)))
+                .reduce((nr1, nr2) -> nr1 * nr2)
+                .get();
+    }
+
+    /***
+     * Find three entries in a list that sum to 2020 and then multiply those three numbers together.
+     *
+     * @param numbers List of Integer
+     * @return product of those three numbers
+     */
+    private int getResultForThreeNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(nr1 -> numbers.stream().anyMatch(nr2 -> numbers.stream().anyMatch(nr3 -> (nr1 + nr2 + nr3 == 2020))))
+                .reduce((nr1, nr2) -> nr1 * nr2)
+                .get();
+    }
 }
---
+++ src/main/java/util/InputReader.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class InputReader {
+
+    public static List<String> readInputFile(String filepath) {
+        List<String> lines = Collections.emptyList();
+        try {
+            Path resourcePath = Paths.get(ClassLoader.getSystemResource(filepath).toURI());
+            lines = Files.readAllLines(Paths.get(resourcePath.toUri()), StandardCharsets.UTF_8);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
