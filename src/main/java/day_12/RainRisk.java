fe2162a15cec14e21cb1e7d13f6a861a39cf33ca
cherryDevBomb <turcan.olguta@gmail.com>
2020-12-16 23:02:50 +0200

    complete day 12 part 2

--- src/main/java/day_12/RainRisk.java
+++ src/main/java/day_12/RainRisk.java
@@ -2,9 +2,11 @@ package day_12;
 
 import util.InputReader;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
@@ -34,8 +36,11 @@ public class RainRisk {
         Map<String, Integer> distances = getTraveledDistances(instructions);
         int manhattanDistance = Math.abs(distances.get(N) - distances.get(S)) + Math.abs(distances.get(E) - distances.get(W));
 
+        Map<String, Integer> waypointDistances = getTraveledDistancesWithWaypoint(instructions);
+        int manhattanDistanceWithWaypoint = Math.abs(waypointDistances.get(N) - waypointDistances.get(S)) + Math.abs(waypointDistances.get(E) - waypointDistances.get(W));
 
         System.out.println("Manhattan distance between the ship's starting position and end location: " + manhattanDistance);
+        System.out.println("Manhattan distance between the ship's starting position and end location with waypoint: " + manhattanDistanceWithWaypoint);
     }
 
     private Map<String, Integer> getTraveledDistances(List<Instruction> instructions) {
@@ -53,11 +58,7 @@ public class RainRisk {
                 .filter(instruction -> !L.equals(instruction.getAction()) && !R.equals(instruction.getAction()))
                 .collect(Collectors.groupingBy(Instruction::getAction, Collectors.summingInt(Instruction::getValue)));
 
-        directions.forEach(dir -> {
-            if (!distances.containsKey(dir)) {
-                distances.put(dir, 0);
-            }
-        });
+        directions.forEach(dir -> distances.computeIfAbsent(dir, x -> 0));
 
         return distances;
     }
@@ -80,5 +81,54 @@ public class RainRisk {
         newDirIndex = newDirIndex % directions.size();
 
         return directions.get(newDirIndex);
+    }
+
+    private Map<String, Integer> getTraveledDistancesWithWaypoint(List<Instruction> instructions) {
+        Map<String, AtomicInteger> waypointCoordinates = directions.stream().collect(Collectors.toMap(d -> d, d -> new AtomicInteger()));
+        waypointCoordinates.get(E).set(10);
+        waypointCoordinates.get(N).set(1);
+
+        Map<String, Integer> distances = instructions.stream()
+                .map(instruction -> {
+                    if (F.equals(instruction.getAction())) {
+                        return waypointCoordinates.entrySet().stream()
+                                .filter(entry -> entry.getValue().get() != 0)
+                                .map(entry -> new Instruction(entry.getKey(), entry.getValue().get() * instruction.getValue()))
+                                .collect(Collectors.toList());
+                    } else if (L.equals(instruction.getAction()) || R.equals(instruction.getAction())) {
+                        Map<String, Integer> currentWaypointCoordinates = waypointCoordinates.entrySet().stream()
+                                .filter(entry -> entry.getValue().get() != 0)
+                                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().get()));
+                        waypointCoordinates.forEach((key, value) -> value.set(0));
+                        currentWaypointCoordinates.forEach((key, value) -> {
+                            String newDirection = getNewFacingDirection(key, instruction);
+                            waypointCoordinates.get(newDirection).set(value);
+                        });
+                    } else {
+                        String moveCoordinate = instruction.getAction();
+                        String oppositeCoordinate = directions.get((directions.indexOf(moveCoordinate) + 2) % directions.size());
+                        if (waypointCoordinates.get(oppositeCoordinate).get() != 0) {
+                            int newEastCoordinate = waypointCoordinates.get(oppositeCoordinate).get() - instruction.getValue();
+                            if (newEastCoordinate > 0) {
+                                waypointCoordinates.get(oppositeCoordinate).set(newEastCoordinate);
+                            } else {
+                                waypointCoordinates.get(moveCoordinate).set(Math.abs(newEastCoordinate));
+                                waypointCoordinates.get(oppositeCoordinate).set(0);
+                            }
+                        } else {
+                            int newEastCoordinate = waypointCoordinates.get(moveCoordinate).get() + instruction.getValue();
+                            waypointCoordinates.get(moveCoordinate).set(newEastCoordinate);
+                        }
+                    }
+                    return new ArrayList<Instruction>();
+                })
+                .filter(list -> !list.isEmpty())
+                .flatMap(List::stream)
+                .filter(instruction -> !L.equals(instruction.getAction()) && !R.equals(instruction.getAction()))
+                .collect(Collectors.groupingBy(Instruction::getAction, Collectors.summingInt(Instruction::getValue)));
+
+        directions.forEach(dir -> distances.computeIfAbsent(dir, x -> 0));
+
+        return distances;
     }
 }
