aac55a4ad9b195b039a1b28a223d3bdb1786f038
cherryDevBomb <turcan.olguta@gmail.com>
2020-12-26 13:15:44 +0200

    refactor day 15

--- src/main/java/day_15/RambunctiousRecitation.java
+++ src/main/java/day_15/RambunctiousRecitation.java
@@ -2,7 +2,6 @@ package day_15;
 
 import util.InputReader;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,14 +23,33 @@ public class RambunctiousRecitation {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        List<Integer> spokenNumbers = new ArrayList<>(startingNumbers);
-        while (spokenNumbers.size() < 2020) {
-            spokenNumbers.add(getNextNumber(spokenNumbers));
-        }
+        int number2020 = getNthNumber(startingNumbers, 2020);
 
-        System.out.println("The 2020th number spoken: " + spokenNumbers.get(spokenNumbers.size() - 1));
+        System.out.println("The 2020th number spoken: " + number2020);
     }
 
+    /**
+     * Calculate the nth number that will be spoken according to the game rules.
+     *
+     * @param spokenNumbers list of starting numbers
+     * @param n             number
+     * @return nth number that will be spoken.
+     */
+    private int getNthNumber(List<Integer> spokenNumbers, int n) {
+        while (spokenNumbers.size() < n) {
+            spokenNumbers.add(getNextNumber(spokenNumbers));
+        }
+        return spokenNumbers.get(spokenNumbers.size() - 1);
+    }
+
+    /**
+     * Given the list of previously spoken numbers, get the number that should be spoken at the next turn, based on the number spoken in the last turn:
+     * If that was the first time the number has been spoken, the current player says 0.
+     * Otherwise, the number had been spoken before; the current player announces how many turns apart the number is from when it was previously spoken.
+     *
+     * @param spokenNumbers list of previously spoken numbers
+     * @return next number that should be spoken
+     */
     private int getNextNumber(List<Integer> spokenNumbers) {
         List<Integer> numberOccurrences = IntStream.range(0, spokenNumbers.size())
                 .filter(turn -> spokenNumbers.get(turn).equals(spokenNumbers.get(spokenNumbers.size() - 1)))
