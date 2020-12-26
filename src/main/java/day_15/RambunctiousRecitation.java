2df38e366f9060a7aa422bdbae2f18abcbe2e72c
cherryDevBomb <turcan.olguta@gmail.com>
2020-12-26 13:57:54 +0200

    complete day 15 part 2

--- src/main/java/day_15/RambunctiousRecitation.java
+++ src/main/java/day_15/RambunctiousRecitation.java
@@ -2,8 +2,10 @@ package day_15;
 
 import util.InputReader;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -24,18 +26,45 @@ public class RambunctiousRecitation {
                 .collect(Collectors.toList());
 
         int number2020 = getNthNumber(startingNumbers, 2020);
+        int number30000000 = getNthNumber(startingNumbers, 30000000);
 
         System.out.println("The 2020th number spoken: " + number2020);
+        System.out.println("The 2020th number spoken: " + number30000000);
+    }
+
+    /**
+     * Calculate the nth number that will be spoken according to the game rules:
+     * If that was the first time the number has been spoken, the current player says 0.
+     * Otherwise, the number had been spoken before; the current player announces how many turns apart the number is from when it was previously spoken.
+     *
+     * @param startingNumbers list of starting numbers
+     * @param n               number
+     * @return nth number that will be spoken.
+     */
+    private int getNthNumber(List<Integer> startingNumbers, int n) {
+        Map<Integer, Integer> occurrences = startingNumbers.stream()
+                .collect(Collectors.toMap(nr -> nr, nr -> startingNumbers.indexOf(nr) + 1));
+
+        int turn = startingNumbers.size();
+        int lastSpokenNumber = startingNumbers.get(startingNumbers.size() - 1);
+        while (turn < n) {
+            int nextNumber = occurrences.containsKey(lastSpokenNumber) ? turn - occurrences.get(lastSpokenNumber) : 0;
+            occurrences.put(lastSpokenNumber, turn++);
+            lastSpokenNumber = nextNumber;
+        }
+        return lastSpokenNumber;
     }
 
     /**
      * Calculate the nth number that will be spoken according to the game rules.
+     * This solution is the not the most optimized and not suitable for part 2.
      *
-     * @param spokenNumbers list of starting numbers
-     * @param n             number
+     * @param startingNumbers list of starting numbers
+     * @param n               number
      * @return nth number that will be spoken.
      */
-    private int getNthNumber(List<Integer> spokenNumbers, int n) {
+    private int getNthNumberWithoutMap(List<Integer> startingNumbers, int n) {
+        List<Integer> spokenNumbers = new ArrayList<>(n);
         while (spokenNumbers.size() < n) {
             spokenNumbers.add(getNextNumber(spokenNumbers));
         }
