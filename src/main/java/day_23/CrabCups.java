e75f690ce2426e979cf74dc2660b640118e1e57e
cherryDevBomb <turcan.olguta@gmail.com>
2021-02-16 18:58:37 +0200

    working version

--- src/main/java/day_23/CrabCups.java
+++ src/main/java/day_23/CrabCups.java
@@ -30,15 +30,15 @@ public class CrabCups {
 //                .map(Object::toString)
 //                .collect(Collectors.joining(""));
 
-//        List<Long> millionCups = new ArrayList<>(inputCups);
-//        millionCups.addAll(LongStream.rangeClosed(10, 1_000_000).boxed().collect(Collectors.toList()));
-//        List<Long> millionGameResult = playGame(millionCups, 10_000_000);
-//        long starProduct = millionGameResult.get(millionGameResult.indexOf(1L) + 1) * millionGameResult.get(millionGameResult.indexOf(1L) + 2);
-
         List<Long> millionCups = new ArrayList<>(inputCups);
         millionCups.addAll(LongStream.rangeClosed(10, 1_000_000).boxed().collect(Collectors.toList()));
-        List<Long> millionGameResult = playGame(millionCups, 100_000);
+        List<Long> millionGameResult = playGame(millionCups, 10_000_000);
         long starProduct = millionGameResult.get(millionGameResult.indexOf(1L) + 1) * millionGameResult.get(millionGameResult.indexOf(1L) + 2);
+
+//        List<Long> millionCups = new ArrayList<>(inputCups);
+//        millionCups.addAll(LongStream.rangeClosed(10, 1_000_000).boxed().collect(Collectors.toList()));
+//        List<Long> millionGameResult = playGame(millionCups, 100_000);
+//        long starProduct = millionGameResult.get(millionGameResult.indexOf(1L) + 1) * millionGameResult.get(millionGameResult.indexOf(1L) + 2);
 
 
 //        System.out.println("Cups after 100 moves: " + finalLabeling);
@@ -51,21 +51,21 @@ public class CrabCups {
         long maxCup = cups.size();
         AtomicBoolean hasRotated = new AtomicBoolean();
 
-        long start = 0;
-        long checkpoint = 0;
-        long pickUpTime = 0;
-        long chooseDestinationTime = 0;
-        long addPickUpTime = 0;
-        long shiftCurrentCupTime = 0;
+//        long start = 0;
+//        long checkpoint = 0;
+//        long pickUpTime = 0;
+//        long chooseDestinationTime = 0;
+//        long addPickUpTime = 0;
+//        long shiftCurrentCupTime = 0;
 
         for (long i = 0; i < nrOfRounds; i++) {
-            start = System.currentTimeMillis();
+//            start = System.currentTimeMillis();
 
             List<Long> pickUp = pickUpCups(cups, currentCup, currentCupIndex, hasRotated);
 
-            checkpoint = System.currentTimeMillis();
-            pickUpTime += checkpoint - start;
-            start = System.currentTimeMillis();
+//            checkpoint = System.currentTimeMillis();
+//            pickUpTime += checkpoint - start;
+//            start = System.currentTimeMillis();
 
             long destinationCup = currentCup - 1;
             while (pickUp.contains(destinationCup) || destinationCup == 0) {
@@ -75,16 +75,16 @@ public class CrabCups {
                 }
             }
 
-            checkpoint = System.currentTimeMillis();
-            chooseDestinationTime += checkpoint - start;
-            start = System.currentTimeMillis();
+//            checkpoint = System.currentTimeMillis();
+//            chooseDestinationTime += checkpoint - start;
+//            start = System.currentTimeMillis();
 
             int destinationCupIndex = cups.indexOf(destinationCup);
             cups.addAll(destinationCupIndex + 1, pickUp);
 
-            checkpoint = System.currentTimeMillis();
-            addPickUpTime += checkpoint - start;
-            start = System.currentTimeMillis();
+//            checkpoint = System.currentTimeMillis();
+//            addPickUpTime += checkpoint - start;
+//            start = System.currentTimeMillis();
 
             // calculating cups.indexOf(currentCup) is not time efficient. It is the only way to calculate next currentCupIndex when the array has rotated.
             // Otherwise, since the pick-up will always have a size of 3 elements, use destinationCupIndex to calculate next currentCupIndex:
@@ -101,21 +101,23 @@ public class CrabCups {
             }
             currentCup = cups.get(currentCupIndex);
 
-            checkpoint = System.currentTimeMillis();
-            shiftCurrentCupTime += checkpoint - start;
+//            checkpoint = System.currentTimeMillis();
+//            shiftCurrentCupTime += checkpoint - start;
 
+            System.out.println("Completed round " + i);
         }
-//            System.out.println("Completed round " + i + " in " + (checkpoint - start));
 
-        System.out.println("pickUpTime = " + pickUpTime);
-        System.out.println("chooseDestinationTime = " + chooseDestinationTime);
-        System.out.println("addPickUpTime = " + addPickUpTime);
-        System.out.println("shiftCurrentCupTime = " + shiftCurrentCupTime);
+        System.out.println("Completed while with all rounds");
 
-        System.out.println("calcFromToTime = " + calcFromToTime);
-        System.out.println("rotateIfOutOfBoundsTime = " + rotateIfOutOfBoundsTime);
-        System.out.println("sublistTime = " + sublistTime);
-        System.out.println("removeTime = " + removeTime);
+//        System.out.println("pickUpTime = " + pickUpTime);
+//        System.out.println("chooseDestinationTime = " + chooseDestinationTime);
+//        System.out.println("addPickUpTime = " + addPickUpTime);
+//        System.out.println("shiftCurrentCupTime = " + shiftCurrentCupTime);
+//
+//        System.out.println("calcFromToTime = " + calcFromToTime);
+//        System.out.println("rotateIfOutOfBoundsTime = " + rotateIfOutOfBoundsTime);
+//        System.out.println("sublistTime = " + sublistTime);
+//        System.out.println("removeTime = " + removeTime);
 
         return cups;
     }
@@ -131,17 +133,17 @@ public class CrabCups {
      * @return list containing the labels of 3 cups following the current cup.
      */
     private List<Long> pickUpCups(List<Long> cups, Long currentCup, int currentCupIndex, AtomicBoolean hasRotated) {
-        long start = 0;
-        long checkpoint = 0;
-
-        start = System.currentTimeMillis();
+//        long start = 0;
+//        long checkpoint = 0;
+//
+//        start = System.currentTimeMillis();
 
         int from = currentCupIndex + 1;
         int to = from + 3;
 
-        checkpoint = System.currentTimeMillis();
-        calcFromToTime += checkpoint - start;
-        start = System.currentTimeMillis();
+//        checkpoint = System.currentTimeMillis();
+//        calcFromToTime += checkpoint - start;
+//        start = System.currentTimeMillis();
 
         //rotate cups if indices are out of bounds
         if (to > cups.size()) {
@@ -156,23 +158,23 @@ public class CrabCups {
             hasRotated.set(true);
         }
 
-        checkpoint = System.currentTimeMillis();
-        rotateIfOutOfBoundsTime += checkpoint - start;
-        start = System.currentTimeMillis();
+//        checkpoint = System.currentTimeMillis();
+//        rotateIfOutOfBoundsTime += checkpoint - start;
+//        start = System.currentTimeMillis();
 
         List<Long> pickUp = new ArrayList<>(cups.subList(from, to));
 
-        checkpoint = System.currentTimeMillis();
-        sublistTime += checkpoint - start;
-        start = System.currentTimeMillis();
+//        checkpoint = System.currentTimeMillis();
+//        sublistTime += checkpoint - start;
+//        start = System.currentTimeMillis();
 
         //this is 35x times faster than removeAll()
         cups.remove(from);
         cups.remove(from);
         cups.remove(from);
 
-        checkpoint = System.currentTimeMillis();
-        removeTime += checkpoint - start;
+//        checkpoint = System.currentTimeMillis();
+//        removeTime += checkpoint - start;
 
         return pickUp;
     }
